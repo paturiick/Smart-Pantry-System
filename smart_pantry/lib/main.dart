3c79686e5bf043bd735faae73140b9dc587ef308
Patrick <rautraut.john19@gmail.com>
2025-05-26 02:28:43 +0800

    fix: fixed white screen

--- smart_pantry/lib/main.dart
+++ smart_pantry/lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_pantry/firebase_options.dart';
 import 'package:smart_pantry/pages/home_page.dart';
 import 'package:smart_pantry/routes/pages.dart';
 
-Future<void> main() async {
-  await Firebase.initializeApp();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,7 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routes: AppPages.getRoutes(),
       theme: ThemeData(
-        fontFamily: 'AvenirNextCyr',
+        fontFamily: 'Poppins',
         scaffoldBackgroundColor: Colors.white,
       ),
       home: HomePage(),
